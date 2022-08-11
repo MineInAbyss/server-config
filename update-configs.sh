7bcf630808e1dd1e92bf7139fa597215a96a79f3
Danielle Voznyy <dan.voznyy@gmail.com>
2022-08-10 23:54:27 -0400

    Rename used file to config-branch

--- update-configs.sh
+++ update-configs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-definedBranch=$(cat pull-branch 2>/dev/null)
+definedBranch=$(cat config-branch 2>/dev/null)
 ansible-pull -U "https://github.com/MineInAbyss/server-config.git" -d "/opt/mineinabyss-configs" --checkout ${definedBranch:=master}
