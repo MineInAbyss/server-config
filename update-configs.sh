0b04cb99f4a9c4778106b8af8754142b1dbd8c89
0ffz <dan.voznyy@gmail.com>
2022-08-11 11:12:14 -0400

    Lowercase v

--- update-configs.sh
+++ update-configs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 definedBranch=$(cat config-branch 2>/dev/null)
-ansible-pull -U "https://github.com/MineInAbyss/server-config.git" -d "/opt/mineinabyss-configs" --checkout ${definedBranch:=master} -V
+ansible-pull -U "https://github.com/MineInAbyss/server-config.git" -d "/opt/mineinabyss-configs" --checkout ${definedBranch:=master} -v
