3b9e6ecc2a600699ac3fb988acfaaa600723383c
Danielle Voznyy <dan.voznyy@gmail.com>
2022-12-28 23:25:01 -0500

    Pull configs branch from env instead of file

--- update-configs.sh
+++ update-configs.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-definedBranch=$(cat config-branch 2>/dev/null)
-ansible-pull -U "https://github.com/MineInAbyss/server-config.git" -d "/home/container/downloaded-configs" --checkout ${definedBranch:=master} -v
+ansible-pull -U "https://github.com/MineInAbyss/server-config.git" -d "/home/container/downloaded-configs" --checkout ${CONFIG_PULL_BRANCH:=master} -v
